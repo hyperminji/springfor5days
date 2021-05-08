852de7e7ab9ce1d9f68bcbac52b090d1a18a8b9a
hyperminji <hyperminji@gmail.com>
2021-05-09 00:12:42 +0900

    210509 day1 try board

--- src/main/java/polymorphism/TVUser.java
+++ src/main/java/polymorphism/TVUser.java
@@ -9,7 +9,7 @@ public class TVUser {
 		
 		
 		//1.spring 컨데이터 구동
-		AbstractApplicationContext factory = new GenericXmlApplicationContext("applicationContext.xml");
+		AbstractApplicationContext factory = new GenericXmlApplicationContext("applicationContext2.xml");
 		/* applicationContext.xml파일 로딩 
 		-> GenericXmlApplicationContext객체 생성 스프링 컨테이너 구동*/
 		
